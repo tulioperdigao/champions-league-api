1a7e04ab8c85cc8997d465db2b181c2625212da1
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-05 15:28:18 -0300

    implementando delete route e controller

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -45,3 +45,7 @@ export const createPlayerService = async (player: PlayerModel) => {
 
     return response;
 }
+
+export const deletePlayerService = async (id:number) => {
+    let response = null;
+}
