fb0b33ff8b69256292828f84d9693554f8eeac6a
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-05 15:44:50 -0300

    implementando upddate route e controller

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,4 +1,5 @@
 import { PlayerModel } from "../models/player-model";
+import { StatisticsModel } from "../models/statistics-model";
 import * as PlayerRepository from "../repositories/players-repository";
 import * as HttpResponse from "../utils/http-helper";
 
@@ -52,4 +53,8 @@ export const deletePlayerService = async (id:number) => {
 
     response = await HttpResponse.ok({message: 'deleted'});
     return response;
+}
+
+export const updatePlayerService = async (id:number, statistics: StatisticsModel) => {
+    
 }
