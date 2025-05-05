9495d202d2c0417475fac0ff0f5536a84fa2dae3
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-05 11:12:07 -0300

    Configurações do express

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
-import express, {Request, Response} from 'express';
+import express, {json, Request, Response} from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
+
+app.use(json())
 
 app.get("/", (req: Request, res:Response) => {
-    res.send("Hello World!")
+    res.status(200).json(
+        {
+            'player': 'Lionel Messi'
+        }
+    );
 })
 
-app.listen(process.env.PORT);
+app.listen(port, () => {
+    console.log(`✅ Server running at port http://localhost:${port}/`);
+});
