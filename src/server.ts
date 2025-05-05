a3fdf44f870d52a1ce84118f9a4a01c08d788166
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-05 11:02:15 -0300

    utilizando o express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res:Response) => {
+    res.send("Hello World!")
+})
+
+app.listen(process.env.PORT);
