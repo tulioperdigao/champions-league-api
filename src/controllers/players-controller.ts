c582fbd0400a9fd5a8ca51bad7f9c1b5fbbd08dd
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-05 11:52:33 -0300

    criando camada de utils

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import { getPlayerService } from "../services/players-service";
+import { ok } from "../utils/http-helper";
 
 export const getPlayer = async (req: Request, res:Response) => {
-
+    
     const data = await getPlayerService();
-    res.status(200).json(data);
+    const response = await ok(data);
+
+    res.status(response.statusCode).json(response.body);
 }
