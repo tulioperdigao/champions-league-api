c2bd929cca95fe049ad2f71b772eeca77c0e5b7b
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2025-05-06 01:20:13 -0300

    lendo arquivo JSON

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,25 +1,8 @@
 import { ClubModel } from "../models/club-model";
-
-
-const database = [
-    { id: 1, name: "Paris Saint-Germain" },
-    { id: 2, name: "Liverpool" },
-    { id: 3, name: "Benfica" },
-    { id: 4, name: "Barcelona" },
-    { id: 5, name: "Real Madrid" },
-    { id: 6, name: "Atlético de Madrid" },
-    { id: 7, name: "Bayer Leverkusen" },
-    { id: 8, name: "Bayern de Munique" },
-    { id: 9, name: "Arsenal" },
-    { id: 10, name: "PSV Eindhoven" },
-    { id: 11, name: "Internazionale" },
-    { id: 12, name: "Borussia Dortmund" },
-    { id: 13, name: "Aston Villa" },
-    { id: 14, name: "Feyenoord" },
-    { id: 15, name: "Club Brugge" },
-    { id: 16, name: "Lille" }
-]
+import fs from "fs/promises";
 
 export const findAllClubs = async (): Promise<ClubModel[]> => {
-    return database;
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8")
+    const clubs: ClubModel[] = JSON.parse(data);
+    return clubs;
 };
